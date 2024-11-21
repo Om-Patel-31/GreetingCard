a659c00593690194e1319cc0bcc5ed1c592dc0f1
Om Patel <om31digital@gmail.com>
2024-11-21 11:31:30 -0500

    first static page done

--- GreetingCard/Form1.cs
+++ GreetingCard/Form1.cs
@@ -24,7 +24,21 @@ namespace GreetingCard
             //declaring pen, brush and font
             Pen pen = new Pen(Color.White, 10);
             SolidBrush brush = new SolidBrush(Color.White);
-            Font font = 'BMW '
+            Font font = new Font("Midnight Minutes", 80);
+
+            //drawing trick or treat
+            g.DrawString("Trick", font, brush, 230, 10);
+            g.DrawString("Or", font, brush, 290, 120);
+            g.DrawString("Treat", font, brush, 230, 210);
+
+            //drawing a horror gravestone
+            g.FillRectangle(brush, 230, 700, 250, 30);
+            g.FillRectangle(brush, 255, 660, 200, 30);
+            g.FillRectangle(brush, 270, 480, 170, 170);
+            g.FillPie(brush, 270, 400, 170, 170, 180, 180);
+            brush.Color = Color.FromArgb(255, 40, 40, 40);
+            font = new Font("Midnight Minutes", 60);
+            g.DrawString("RIP", font, brush, 290, 450);
         }
     }
 }
