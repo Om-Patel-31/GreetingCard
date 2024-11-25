5497123445360d39372a18ddac94dd4afacec839
Om Patel <om31digital@gmail.com>
2024-11-25 08:50:53 -0500

    works. last page to be done

--- GreetingCard/Form1.cs
+++ GreetingCard/Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -107,17 +110,18 @@ namespace GreetingCard
             //declaring pen, brush and font
             Pen pen = new Pen(Color.White, 10);
             SolidBrush brush = new SolidBrush(Color.White);
-            Font font = new Font("October Crow", 60);
+            Font font = new Font("Trebuchet MS", 60);
+
+            //sound player
+            SoundPlayer ghostPlayer = new SoundPlayer(Properties.Resources.ghost);
+            SoundPlayer batPlayer = new SoundPlayer(Properties.Resources.batVoice);
+            SoundPlayer catPlayer = new SoundPlayer(Properties.Resources.catVoice);
 
             //Clearing the screen
             Refresh();
             g.Clear(Color.FromArgb(255, 40, 40, 40));
 
-            //drawing halloween greeting
-            g.DrawString("Happy Halloween!", font, brush, 80, 50);
-
-            //drawing
-
+            Thread.Sleep(1000);
             //drawing pumpkin
             brush.Color = Color.FromArgb(255, 255, 98, 0);
             g.FillEllipse(brush, 260, 580, 70, 140);
@@ -133,16 +137,18 @@ namespace GreetingCard
             g.DrawLine(pen, 355, 590, 355, 550);
             g.FillPie(brush, 320, 564, 70, 30, 45, 90);
 
+            //drawing treat to trick them to fall for it
+            font = new Font("Trebuchet MS", 100);
+            brush.Color = Color.White;
+            g.DrawString("TREAT!", font, brush, 130, 100);
+
             //making pumpkin's expression
             brush.Color = Color.Black;
             //right eye
-            //g.FillPie(brush, 290, 600, 50, 50, 50, 180);
             g.FillPie(brush, 290, 600, 50, 50, 180, 360);
             brush.Color = Color.White;
             g.FillPie(brush, 295, 605, 30, 30, 180, 360);
             //left eye
-            //evil eye
-            //g.FillPie(brush, 370, 600, 50, 50, -50, 180);
             brush.Color = Color.Black;
             g.FillPie(brush, 370, 600, 50, 50, 180, 360);
             brush.Color = Color.White;
@@ -152,9 +158,203 @@ namespace GreetingCard
             g.FillPie(brush, 330, 620, 50, 40, 60, 65);
             //mouth
             g.FillPie(brush, 320, 640, 70, 70, 0, 180);
-            //brush.Color = Color.FromArgb(255, 255, 135, 7);
-            //g.FillRectangle(brush, 335, 670, 13, 13);
-            //g.FillRectangle(brush, 360, 670, 13, 13);
+
+            //candy border
+            brush.Color = Color.White;
+            g.FillEllipse(brush, 297, 347, 105, 105);
+            //candy
+            brush.Color = Color.Red;
+            g.FillEllipse(brush, 300, 350, 100, 100);
+            brush.Color = Color.White;
+            g.FillPie(brush, 255, 355, 90, 100, 90, 150);
+            g.FillPie(brush, 355, 345, 90, 100, -90, 150);
+            pen.Width = 3;
+            pen.Color = Color.White;
+            //detailing
+            pen.Width = 25;
+            g.DrawLine(pen, 300, 410, 400, 390);
+            brush.Color = Color.Red;
+            g.FillEllipse(brush, 305, 400, 15, 15);
+            g.FillEllipse(brush, 325, 397, 15, 15);
+            g.FillEllipse(brush, 345, 393, 15, 15);
+            g.FillEllipse(brush, 365, 389, 15, 15);
+            g.FillEllipse(brush, 383, 385, 15, 15);
+
+            //making it sleep before tricking them
+            Thread.Sleep(3000);
+
+            g.Clear(Color.FromArgb(255, 40, 40, 40));
+            brush.Color = Color.White;
+            //drawing text
+            font = new Font("October Crow", 100);
+            g.DrawString("Trick!", font, brush, 170, 100);
+
+            //drawing pumpkin
+            brush.Color = Color.FromArgb(255, 255, 98, 0);
+            g.FillEllipse(brush, 260, 580, 70, 140);
+            g.FillEllipse(brush, 380, 580, 70, 140);
+            brush.Color = Color.FromArgb(255, 255, 111, 0);
+            g.FillEllipse(brush, 355, 580, 70, 145);
+            g.FillEllipse(brush, 285, 580, 70, 145);
+            brush.Color = Color.FromArgb(255, 255, 135, 7);
+            g.FillEllipse(brush, 320, 580, 70, 150);
+            pen.Width = 20;
+            pen.Color = Color.FromArgb(255, 4, 125, 0);
+            brush.Color = Color.FromArgb(255, 4, 125, 0);
+            g.DrawLine(pen, 355, 590, 355, 550);
+            g.FillPie(brush, 320, 564, 70, 30, 45, 90);
+            //drawing it's expressions
+            //right eye
+            brush.Color = Color.Black;
+            g.FillPie(brush, 290, 600, 50, 50, 50, 180);
+            //left eye
+            g.FillPie(brush, 370, 600, 50, 50, -50, 180);
+            //nose
+            g.FillPie(brush, 330, 620, 50, 40, 60, 65);
+            //mouth
+            g.FillPie(brush, 320, 640, 70, 70, 0, 180);
+            brush.Color = Color.FromArgb(255, 255, 135, 7);
+            g.FillRectangle(brush, 335, 670, 13, 13);
+            g.FillRectangle(brush, 360, 670, 13, 13);
+
+            //ghost
+            brush.Color = Color.WhiteSmoke;
+            //variables
+            int mainCircleX = 500;
+            int mainRectangleX = 500;
+            int whiteCircle1X = 500;
+            int whiteCircle2X = 545;
+            int whiteCircle3X = 595;
+            int grayCircle1X = 525;
+            int grayCircle2X = 575;
+            int rightEyeX = 515;
+            int leftEyeX = 565;
+            int mouthX = 545;
+            //figure
+            g.FillEllipse(brush, mainCircleX, 300, 125, 100); //ghost figure circle
+            g.FillRectangle(brush, mainRectangleX, 350, 125, 125); //ghost figure square
+            g.FillEllipse(brush, whiteCircle1X, 460, 30, 30); //ghost white first circle
+            g.FillEllipse(brush, whiteCircle2X, 460, 30, 30); //ghost white second circle
+            g.FillEllipse(brush, whiteCircle3X, 460, 30, 30); //ghost white third circle
+            brush.Color = Color.FromArgb(255, 40, 40, 40);
+            g.FillEllipse(brush, grayCircle1X, 460, 20, 30); //ghost gray first oval
+            g.FillEllipse(brush, grayCircle2X, 460, 20, 30); //ghost gray second oval
+            //expressions
+            brush.Color = Color.Black;
+            g.FillPie(brush, rightEyeX, 330, 40, 40, 50, 180); //ghost right eye
+            g.FillPie(brush, leftEyeX, 330, 40, 40, -50, 180); //ghost left eye
+            g.FillEllipse(brush, mouthX, 370, 30, 40); //ghost mouth
+
+            ghostPlayer.Play();
+            //while loop for animating pumpkin and ghost
+            while ((mainCircleX > 20) && (mainRectangleX > 20) && (whiteCircle1X > 20) && (whiteCircle2X > 20) && (whiteCircle3X > 20) && (grayCircle1X > 20) && (grayCircle2X > 20) && (rightEyeX > 20) && (leftEyeX > 20) && (mouthX > 20))
+            {
+                Thread.Sleep(500);
+                mainCircleX -= 20;
+                mainRectangleX -= 20;
+                whiteCircle1X -= 20;
+                whiteCircle2X -= 20;
+                whiteCircle3X -= 20;
+                grayCircle1X -= 20;
+                grayCircle2X -= 20;
+                rightEyeX -= 20;
+                leftEyeX -= 20;
+                mouthX -= 20;
+                Refresh();
+
+                brush.Color = Color.White;
+                //drawing text
+                font = new Font("October Crow", 100);
+                g.DrawString("Trick!", font, brush, 170, 100);
+
+                //drawing pumpkin
+                brush.Color = Color.FromArgb(255, 255, 98, 0);
+                g.FillEllipse(brush, 260, 580, 70, 140);
+                g.FillEllipse(brush, 380, 580, 70, 140);
+                brush.Color = Color.FromArgb(255, 255, 111, 0);
+                g.FillEllipse(brush, 355, 580, 70, 145);
+                g.FillEllipse(brush, 285, 580, 70, 145);
+                brush.Color = Color.FromArgb(255, 255, 135, 7);
+                g.FillEllipse(brush, 320, 580, 70, 150);
+                pen.Width = 20;
+                pen.Color = Color.FromArgb(255, 4, 125, 0);
+                brush.Color = Color.FromArgb(255, 4, 125, 0);
+                g.DrawLine(pen, 355, 590, 355, 550);
+                g.FillPie(brush, 320, 564, 70, 30, 45, 90);
+                //drawing it's expressions
+                //right eye
+                brush.Color = Color.Black;
+                g.FillPie(brush, 290, 600, 50, 50, 50, 180);
+                //left eye
+                g.FillPie(brush, 370, 600, 50, 50, -50, 180);
+                //nose
+                g.FillPie(brush, 330, 620, 50, 40, 60, 65);
+                //mouth
+                g.FillPie(brush, 320, 640, 70, 70, 0, 180);
+                brush.Color = Color.FromArgb(255, 255, 135, 7);
+                g.FillRectangle(brush, 335, 670, 13, 13);
+                g.FillRectangle(brush, 360, 670, 13, 13);
+
+                //figure
+                brush.Color = Color.White;
+                g.FillEllipse(brush, mainCircleX, 300, 125, 100); //ghost figure circle
+                g.FillRectangle(brush, mainRectangleX, 350, 125, 125); //ghost figure square
+                g.FillEllipse(brush, whiteCircle1X, 460, 30, 30); //ghost white first circle
+                g.FillEllipse(brush, whiteCircle2X, 460, 30, 30); //ghost white second circle
+                g.FillEllipse(brush, whiteCircle3X, 460, 30, 30); //ghost white third circle
+                brush.Color = Color.FromArgb(255, 40, 40, 40);
+                g.FillEllipse(brush, grayCircle1X, 460, 20, 30); //ghost gray first oval
+                g.FillEllipse(brush, grayCircle2X, 460, 20, 30); //ghost gray second oval
+                                                                 //expressions
+                brush.Color = Color.Black;
+                g.FillPie(brush, rightEyeX, 330, 40, 40, 50, 180); //ghost right eye
+                g.FillPie(brush, leftEyeX, 330, 40, 40, -50, 180); //ghost left eye
+                g.FillEllipse(brush, mouthX, 370, 30, 40); //ghost mouth
+
+                //changing pumpkin's expression colors
+                Random rand = new Random();
+                int red, green, blue;
+
+                // Generate a color that is not orange
+                while (1 > 0)
+                {
+                    red = rand.Next(0, 256);
+                    green = rand.Next(0, 256);
+                    blue = rand.Next(0, 256);
+
+                    // Check if the color is not orange
+                    if (!(red > 200 && green > 100 && green == 200 && blue == 100))
+                        break;
+                }
+
+                brush.Color = Color.FromArgb(255, red, green, blue);
+                g.FillPie(brush, 290, 600, 50, 50, 50, 180); // left eye
+                g.FillPie(brush, 370, 600, 50, 50, -50, 180); // right eye
+                g.FillPie(brush, 330, 620, 50, 40, 60, 65); // nose
+                g.FillPie(brush, 320, 640, 70, 70, 0, 180);//mouth
+                brush.Color = Color.FromArgb(255, 255, 135, 7);
+                //editing mouth shapes
+                g.FillRectangle(brush, 335, 670, 13, 13);
+                g.FillRectangle(brush, 360, 670, 13, 13);
+            }
+            Refresh();
+
+            brush.Color = Color.White;
+            font = new Font("Midnight Minutes", 80);
+            g.DrawString("Happy", font, brush, 210, 200);
+            g.DrawString("Halloween", font, brush, 150, 320);
+
+
+            //cat
+            brush.Color = Color.Black;
+            catPlayer.Play();
+            //right ear
+            g.FillPie(brush, 615, 470, 125, 125, 100, 175);
+            g.FillPie(brush, 630, 470, 50, 115, 100, -175);
+            g.FillRectangle(brush, 500, 500, 175, 100);
+
+            //bat
+            batPlayer.Play();
         }
     }
 }
